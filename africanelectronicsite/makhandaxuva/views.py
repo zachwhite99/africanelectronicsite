b5dc8fc9895aa33538b5a9aae30ade56bce4a167
thomassobolik <ts3bj@virginia.edu>
2019-04-11 17:10:55 -0400

    created django app, just prints hello world

---
+++ africanelectronicsite/makhandaxuva/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+from django.template import loader
+from django.urls import reverse
+import urllib.request
+import urllib.parse
+import json
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("hello world!")
